7479cb38c81e232863e3fe8d804cb24d72bf1c29
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-02-16 20:24:37 +0100

    Edit def index, add possible filters and categories to show

--- Sklep/Produkty/views.py
+++ Sklep/Produkty/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Produkty
+from .models import Produkty,Kategoria
 
 def index(request):
-    zapytanie = Produkty.objects.all()
-    return HttpResponse(zapytanie)
+    wszystkie = Produkty.objects.all()
+    jeden     = Produkty.objects.get(pk=4)
+    kat       = Produkty.objects.filter(kategoria=2)
+    producent = Produkty.objects.filter(producent=2)
+    kat_name  = Kategoria.objects.get(pk=1)
+    null      = Produkty.objects.filter(kategoria__isnull=True)
+    zawiera   = Produkty.objects.filter(opis__icontains="mysz")
+    return HttpResponse(wszystkie)
