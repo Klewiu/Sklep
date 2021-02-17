5460061d44600dd6559a926e8a61dd030170c70c
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-02-17 22:09:40 +0100

    Create new urls kategoria&produty,  create template with szablon.html that shows kategories name

--- Sklep/Produkty/views.py
+++ Sklep/Produkty/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Produkty,Kategoria
+from .models import Produkty, Kategoria
 
 def index(request):
-    wszystkie = Produkty.objects.all()
-    jeden     = Produkty.objects.get(pk=4)
-    kat       = Produkty.objects.filter(kategoria=2)
-    producent = Produkty.objects.filter(producent=2)
-    kat_name  = Kategoria.objects.get(pk=1)
-    null      = Produkty.objects.filter(kategoria__isnull=True)
-    zawiera   = Produkty.objects.filter(opis__icontains="mysz")
-    return HttpResponse(wszystkie)
+    # jeden     = Produkty.objects.get(pk=4)
+    # kat       = Produkty.objects.filter(kategoria=2)
+    # producent = Produkty.objects.filter(producent=2)
+    # kat_name  = Kategoria.objects.get(pk=1)
+    # null      = Produkty.objects.filter(kategoria__isnull=True)
+    # zawiera   = Produkty.objects.filter(opis__icontains="mysz")
+
+    kategorie= Kategoria.objects.all()
+    dane = {'kategorie' : kategorie}
+    return render(request, 'szablon.html', dane)
+
+def kategoria(request, id):
+    kategoria_user = Kategoria.objects.get(pk = id)
+    return HttpResponse(kategoria_user.nazwa)
+
+def produkt(request,id):
+    produkt_user = Produkty.objects.get(pk=id)
+    napis = "<h1>" + str(produkt_user) + "</h1>" + \
+            "<p>"  + str(produkt_user.opis) + "</p>" + \
+            "<p>"  + str(produkt_user.cena) + "</p>"
+    return HttpResponse(napis)
--- Sklep/Sklep/urls.py
+++ Sklep/Sklep/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Produkty.views import index
+from Produkty.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index, name = 'index'),
+    path('kategoria/<id>/', kategoria, name='kategoria'),
+    path('produkt/<id>/', produkt, name='produkt'),
 ]
