ca4f1d25cedc0b7e7dbfe36b94d59a64f58cbb39
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-02-18 22:08:57 +0100

    Create html szablon.html, produkt.html , kategoria_produkt.html and style.css ; add shortcut icon

--- Sklep/Produkty/views.py
+++ Sklep/Produkty/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Produkty, Kategoria
 
+
 def index(request):
     # jeden     = Produkty.objects.get(pk=4)
     # kat       = Produkty.objects.filter(kategoria=2)
@@ -10,17 +11,23 @@ def index(request):
     # null      = Produkty.objects.filter(kategoria__isnull=True)
     # zawiera   = Produkty.objects.filter(opis__icontains="mysz")
 
-    kategorie= Kategoria.objects.all()
-    dane = {'kategorie' : kategorie}
+    kategorie = Kategoria.objects.all()
+    dane = {'kategorie': kategorie}
     return render(request, 'szablon.html', dane)
 
-def kategoria(request, id):
-    kategoria_user = Kategoria.objects.get(pk = id)
-    return HttpResponse(kategoria_user.nazwa)
 
-def produkt(request,id):
+def kategoria(request, id):
+    kategoria_user = Kategoria.objects.get(pk=id)
+    kategoria_produkt = Produkty.objects.filter(kategoria = kategoria_user)
+    kategorie = Kategoria.objects.all()
+    dane={"kategoria_user": kategoria_user,
+          "kategoria_produkt": kategoria_produkt,
+          "kategorie" : kategorie}
+    return render(request,'kategoria_produkt.html',dane)
+
+
+def produkt(request, id):
     produkt_user = Produkty.objects.get(pk=id)
-    napis = "<h1>" + str(produkt_user) + "</h1>" + \
-            "<p>"  + str(produkt_user.opis) + "</p>" + \
-            "<p>"  + str(produkt_user.cena) + "</p>"
-    return HttpResponse(napis)
+    kategorie = Kategoria.objects.all()
+    dane = {'produkt_user': produkt_user, 'kategorie': kategorie}
+    return render (request, 'produkt.html', dane)
